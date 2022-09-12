db198deee144656f5cb0c38a0ebfc91fac9b2f75
fakhirpy <trjakira@proton.me>
2022-09-12 08:42:07 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+def scan(host, port):
+    try: 
+        for port in port:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(0.5)
+            code = client.connect_ex((host, int(port)))
+            if code == 0:
+                print ("[+] {} open".format(port))
+    except:
+        print("Error something is wrong.")
+        
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        host = sys.argv[1]
+        if len(sys.argv) > 3:
+            ports = sys.argv[2].split(',')
+        else:
+            ports = [21, 22, 23, 25, 80, 443, 8080, 8443, 3306, 139, 135]
+            
+        scan(host, ports)
+    else: print("Usage: python main.py google.com 22, 23, 80, 443") 
